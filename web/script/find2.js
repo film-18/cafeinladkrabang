a0c9bd6b1ac277083bf3225244856b5b21376ba5
film-kanlaya <62070011@it.kmitl.ac.th>
2019-10-26 08:25:21 +0700

    update

--- web/script/find2.js
+++ web/script/find2.js
@@ -10,21 +10,62 @@ var japanStyle = null;
 
 function search() {
     let choice = document.getElementById('first');
-    choice.innerHTML = 'checked!'
+    // choice.innerHTML = 'checked!'
     var box = document.getElementsByClassName('change')[0];
-    box.style.backgroundColor = '#f39c12';
-
+    box.style.backgroundColor = '#f39c12'; 
 }
 
 function search2() {
-    let choice2 = document.getElementById('second')
+    let choiceTwo = document.getElementById('second');
     var box2 = document.getElementsByClassName('change')[1];
-    box2.style.backgroundColor = '#f39c12'; 
+    box.style.backgroundColor = '#f39c12';
+    // if (box2.style.backgroundColor = '#27ae60') {
+    //     box2.style.backgroundColor = 'yellow';
+    // }
+    // else if (box2.style.backgroundColor = 'yellow') {
+    //     box2.style.backgroundColor = '#27ae60';
+    // }
 }
 
 function search3() {
-    let choice3 = document.getElementById('third')
+    let choice3 = document.getElementById('third');
     var box3 = document.getElementsByClassName('change')[2];
     box3.style.backgroundColor = '#f39c12';
 
+}
+
+function search4() {
+    let choice4 = document.getElementById('four');
+    var box4 = document.getElementsByClassName('change')[3];
+    box4.style.backgroundColor = '#f39c12';
+}
+
+function search5() {
+    let choice5 = document.getElementById('five');
+    var box5 = document.getElementsByClassName('change')[4];
+    box5.style.backgroundColor = '#f39c12';
+}
+
+function search6() {
+    let choice6 = document.getElementById('six');
+    var box6 = document.getElementsByClassName('change')[5];
+    box6.style.backgroundColor = '#f39c12';
+}
+
+function search7() {
+    let choice7 = document.getElementById('seven');
+    var box7 = document.getElementsByClassName('change')[6];
+    box7.style.backgroundColor = '#f39c12';
+}
+
+function search8() {
+    let choice8 = document.getElementById('eight');
+    var box8 = document.getElementsByClassName('change')[7];
+    box8.style.backgroundColor = '#f39c12';
+}
+
+function search9() {
+    let choice9 = document.getElementById('nine');
+    var box9 = document.getElementsByClassName('change')[8];
+    box9.style.backgroundColor = '#f39c12';
 }
