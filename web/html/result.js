3537cc1d3d771b088a3f32979a9dc6f26754427e
it62070072 <62070072@it.kmitl.ac.th>
2019-11-26 19:46:01 +0700

    เพิ่มเงื่อนไขให้ครบ

--- web/html/result.js
+++ web/html/result.js
@@ -7,28 +7,58 @@ function load() {
     
     
     let choice = {
-        "photoloftfourty" : "Timber",
-        "readworkloftfourty" : "Timber",
+        "photoloftfifty" : "Timber",
+        "readworkloftfifty" : "Timber",
         "familystreetfifty" : "Nalatkrabang",
         "photoloftfifty" : "Beeloft cafe",
-        "readwordloftsixty" : "Bissocafe",
-        "familyloftsixty" : "Bissocafe",
-        "friendmodernfourty" : "Thinkmilk",
-        "photolofteighty" : "Spacetime Art Galleria",
-        "readandworklofteighty" : "Spacetime Art Galleria",
-        "photoplanteighty" : "Spacetime Art Galleria",
-        "readandworkplanteighty" : "Spacetime Art Galleria",
-        "friendstreetfourty" : "Nom-mahalai",
-        "photoloftsixty" : "Munwhan coffee cafe",
-        "friendloftsixty" : "Munwhan coffee cafe",
-        "readandworkloftsixty" : "Munwhan coffee cafe",
+        "readandworkloftseventy" : "Bissocafe",
+        "familyloftseventy" : "Bissocafe",
+        "friendloftfifty" : "Thinkmilk",
+        "photoloftseventy" : "Spacetime Art Galleria",
+        "readandworkloftseventy" : "Spacetime Art Galleria",
+        "photoplantseventy" : "Spacetime Art Galleria",
+        "readandworkplantseventy" : "Spacetime Art Galleria",
+        "friendstreetfifty" : "Nom-mahalai",
+        "photoloftseventy" : "Munwhan coffee cafe",
+        "friendloftseventy" : "Munwhan coffee cafe",
+        "readandworkloftseventy" : "Munwhan coffee cafe",
         "familypastelfifty" : "Whanyenpensuk",
         "readandworkplantonehun" : "Varvana",
         "friendplantonehun" : "Varvana",
-        "photoloftfifty" : "WE DON'T HAVE FOR YOU",
-        "photoloftsixty" : "WE DON'T HAVE FOR YOU",
-        "photolofteighty" : "WE DON'T HAVE FOR YOU",
         "photoloftonehun" : "WE DON'T HAVE FOR YOU",
+        "photostreetfifty" : "WE DON'T HAVE FOR YOU",
+        "photostreetseventy" : "WE DON'T HAVE FOR YOU",
+        "photostreetonehun" : "WE DON'T HAVE FOR YOU",
+        "photoplantfifty" : "WE DON'T HAVE FOR YOU",
+        "photoplantonehun" : "WE DON'T HAVE FOR YOU",
+        "photopastelfifty" : "WE DON'T HAVE FOR YOU",
+        "photopastelseventy" : "WE DON'T HAVE FOR YOU",
+        "photopastelonehun" : "WE DON'T HAVE FOR YOU",
+        "readandworkloftonehun" : "WE DON'T HAVE FOR YOU",
+        "readandworkstreetfifty" : "WE DON'T HAVE FOR YOU",
+        "readandworkstreetseventy" : "WE DON'T HAVE FOR YOU",
+        "readandworkstreetonehun" : "WE DON'T HAVE FOR YOU",
+        "readandworkplantfifty" : "WE DON'T HAVE FOR YOU",
+        "readandworkpastelfifty" : "WE DON'T HAVE FOR YOU",
+        "readandworkpastelseventy" : "WE DON'T HAVE FOR YOU",
+        "readandworkpastelonehun" : "WE DON'T HAVE FOR YOU",
+        "friendloftonehun" : "WE DON'T HAVE FOR YOU",
+        "friendstreetseventy" : "WE DON'T HAVE FOR YOU",
+        "friendstreetonehun" : "WE DON'T HAVE FOR YOU",
+        "friendplantfifty" : "WE DON'T HAVE FOR YOU",
+        "friendplantseventy" : "WE DON'T HAVE FOR YOU",
+        "friendpastelfifty" : "WE DON'T HAVE FOR YOU",
+        "friendpastelseventy" : "WE DON'T HAVE FOR YOU",
+        "friendpastelonehun" : "WE DON'T HAVE FOR YOU",
+        "familyloftfifty" : "WE DON'T HAVE FOR YOU",
+        "familyloftonehun" : "WE DON'T HAVE FOR YOU",
+        "familystreetseventy" : "WE DON'T HAVE FOR YOU",
+        "familystreetonehun" : "WE DON'T HAVE FOR YOU",
+        "familyplantfifty" : "WE DON'T HAVE FOR YOU",
+        "familyplantseventy" : "WE DON'T HAVE FOR YOU",
+        "familyplantonehun" : "WE DON'T HAVE FOR YOU",
+        "familypastelseventy" : "WE DON'T HAVE FOR YOU",
+        "familypastelonehun" : "WE DON'T HAVE FOR YOU",
 
     }
     
@@ -36,53 +66,53 @@ function load() {
         "Timber": {
             "name": "Timber",
             "description": "",
-            "opentime": ""
+            "opentime": "11:00 - 22:00"
         },
 
         "Nalatkrabang" : {
             "name": "Nalatkrabang",
             "description": "",
-            "opentime": ""
+            "opentime": "10:00 - 19:00"
         },
         "Beeloft cafe" : {
             "name": "Beeloft cafe",
             "description": "",
-            "opentime": ""
+            "opentime": "09:00 - 21:00"
         },
         "Bissocafe" : {
             "name": "Bissocafe",
             "description": "",
-            "opentime": ""
+            "opentime": "07.00 - 18.00"
         },
         "Thinkmilk" : {
             "name": "Thinkmilk",
             "description": "",
-            "opentime": ""
+            "opentime": "16:00 - 00:00"
         },
         "Spacetime Art Galleria" : {
             "name": "Spacetime Art Galleria",
             "description": "",
-            "opentime": ""
+            "opentime": "08:00 - 21:00"
         },
         "Munwhan coffee cafe" : {
             "name": "Munwhan coffee cafe",
             "description": "",
-            "opentime": ""
+            "opentime": "06:00 - 19:00"
         },
         "Nom-mahalai" : {
             "name": "Nom-mahalai",
             "description": "",
-            "opentime": ""
+            "opentime": "15:00 - 00:00"
         },
         "Varvana" : {
             "name": "Varvana",
             "description": "",
-            "opentime": ""
+            "opentime": "11:30-21:30"
         },
         "Whanyenpensuk" : {
             "name": "Whanyenpensuk",
             "description": "",
-            "opentime": ""
+            "opentime": "17:00 - 22:30"
         },
         "WE DON'T HAVE FOR YOU" : {
             "name" : "WE DON'T HAVE FOR YOU",
